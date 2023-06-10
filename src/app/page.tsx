e5c346953254f872346b77e68cf520084b6047be
ByteDance <gengzhaoyang@bytedance.com>
2023-06-10 19:15:44 +0800

    feat: 主体完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from 'next/image'
+"use client";
+
+import { WalletProvider } from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
+import { ConnectButton } from '@suiet/wallet-kit';
+import { useWallet } from '@suiet/wallet-kit';
+import { TransactionBlock } from "@mysten/sui.js";
+import TransferForm from '@/compnents/transfer-form';
+import { Layout, Card, message } from 'antd';
+import styles from './page.module.scss';
+import SuiIcon from '@/compnents/sui-icon';
+import { Select, Form, Button, Row } from 'antd';
+
+
+
+
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const wallet = useWallet();
+  const [form] = Form.useForm();
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const onFinish = async (values: { transaction: Array<{ address: string, amount: number }> }) => {
+    const txb = new TransactionBlock();
+    values.transaction.forEach(item => {
+      const valiCount = item.amount * 10**9; 
+      const [coin] = txb.splitCoins(txb.gas, [txb.pure(valiCount)]);
+      txb.transferObjects([coin], txb.pure(item.address));
+    });
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const res = await wallet.signAndExecuteTransactionBlock({
+      transactionBlock: txb,
+    });
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+   if(res.confirmedLocalExecution){
+    message.success(<div>disperse success, see detail in <a  target='_blank' href={`https://suiexplorer.com/txblock/${res.digest}?network=devnet`}>sui explorer</a></div>, 60)
+   }
+  };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+
+
+  return <Layout className={styles.layout}>
+
+        <Form form={form} onFinish={onFinish}>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <div className={styles.icon}><SuiIcon ></SuiIcon></div>
+              <h1 >disperse<sup>devnet</sup></h1>
+            </div>
+            <p className={styles.dsc} >distribute sui or tokens to multiple addresses</p>
+
+            <Card className={styles.card}>
+              <h2 className={styles.cardHeader} >connect to wallet</h2>
+              < ConnectButton />
+            </Card>
+
+            <Card className={styles.card}>
+              <h2 className={styles.cardHeader} >choose token</h2>
+              <Form.Item name="type" initialValue="sui">
+              <Select disabled  options={[{ label: 'sui', value: 'sui' }, { label: 'token', value: 'token' }]}  style={{ width: '160px' }}></Select>
+              </Form.Item>
+            </Card>
+
+
+            <Card className={styles.card}>
+              <h2 className={styles.cardHeader} >recipients and amounts</h2>
+              <p className={styles.para}> enter one address and amount  on each line.</p>
+              <TransferForm />
+            </Card>
+
+
+           <div  className={styles.submit}>
+              <Button type="primary" htmlType="submit">
+                confirm disperse
+              </Button>
+            </div>
+
+
+          </div>
+
+
+         
+        </Form>
+
+
+
+      <Footer style={{ textAlign: 'center' }} >sui disperse Created by xiguakaoya ©2023</Footer>
+    </Layout>
+
+
 }
---
+++ src/compnents/transfer-form.tsx
@@ -0,0 +1,64 @@
+import { Form, Input, InputNumber, Button, Space, Row, Col } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { useWallet } from '@suiet/wallet-kit';
+
+
+export default function TransferForm() {
+    const [form] = Form.useForm();
+    const wallet = useWallet();
+
+   
+
+
+    return<>
+        <Form.List
+            initialValue={[{ address: '', amount: null }]}
+            name="transaction">
+            {
+                (fields, { add, remove }, { errors }) => (
+
+                    <>
+                        {
+                            fields.map(({ key, name, ...restField }, index) => (
+                                <Row key={key} style={{ display: 'flex', marginBottom: 8 }} >
+                                    <Col span={16}>
+                                    <Form.Item  rules={[{ required: true, message: '请填写地址' }]} label="address"  {...restField} name={[name, 'address']} >
+                                        <Input />
+                                    </Form.Item>
+                                    </Col>
+                                    <Col span={8}>
+                                    <Form.Item rules={[{ required: true, message: '请填写数量' }]}   label="amount"  {...restField} name={[name, 'amount']} >
+                                    <InputNumber></InputNumber>
+                                    </Form.Item>
+                                    </Col>
+                                    {fields.length > 1 ? (
+                                        <MinusCircleOutlined
+                                            className="dynamic-delete-button"
+                                            onClick={() => remove(name)}
+                                        />
+                                    ) : null}
+                                </Row>
+                            ))
+                        }
+
+                        <Form.Item>
+                            <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                                
+                            </Button>
+                        </Form.Item>
+
+
+                    </>
+                )
+            }
+
+        </Form.List>
+
+
+        </>
+   
+
+
+
+
+}
